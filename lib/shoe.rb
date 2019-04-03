c999f89670d087386bc7c07d39f577a5e13e7237
Ad Friedman <adam.friedman@flatironschool.com>
2019-04-03 16:22:27 -0400

    13th RSpec's answer is unexplained

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,38 @@
-# Make your shoe class here!
+class Shoe
+attr_accessor :brand, :color, :size, :material, :condition
+
+def initialize(brand)
+  @brand = brand
+end
+
+def cobble (condition="old")
+  puts "Your shoe is as good as new!"
+  @condition = "new"
+end
+
+# def cobble
+# puts "The shoe has been repaired"
+# end
+
+# def color=(color)
+#   @color = color
+# end
+#
+# def color
+#   @color
+# end
+
+# def size=(size)
+#   @size = size
+# end
+#
+# def size
+#   @size
+# end
+
+
+
+
+
+
+end
