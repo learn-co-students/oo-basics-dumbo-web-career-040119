8e030fd38ec218c186be9b6dcae6fdb230ecb682
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 02:04:04 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe
+  
+  attr_accessor ;, :brand, :color, :size, :material, :condition
+  
+  def initialize(brand) 
+    @brand = brand
+    @color
+    @size
+    @material
+    @condition
+  end
+
+  def cobble
+    puts "Flipping the page...wow, you read fast!"
+    @condition = 'new'
+  end
+
+end
+
